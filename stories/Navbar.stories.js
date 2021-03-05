0af22592485afeec679fca9d0cae15e481f317da
AlexanderInglis <alexinglis10@gmail.com>
2021-03-04 18:57:43 -0800

    created NavBar

---
+++ stories/Navbar.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import BottomNav from "../comps/NavBar";
+
+export default {
+  title: "example/NavBar",
+  component: BottomNav,
+};
+
+export const BottomNavigation = () => <BottomNav></BottomNav>;
