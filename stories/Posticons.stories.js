12c74c3d5fe737dfb1246c44d433406ca396097d
AlexanderInglis <alexinglis10@gmail.com>
2021-03-10 17:40:55 -0800

    finished making the like button

--- stories/Posticons.stories.js
+++ stories/Posticons.stories.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import Posticons from '../comps/Posticons';
-
-
+import React from "react";
+import LikeButton from "../comps/LikeButton";
+import Posticons from "../comps/Posticons";
 
 export default {
-    title: 'Icon',
-    };
+	title: "Icon",
+};
 
-    export const EditPostSection = () => (
-        <Posticons 
-        />
-      );
-   
+export const EditPostSection = () => <Posticons />;
+
+export const likeBtn = () => <LikeButton />;
